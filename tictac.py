9e8dcb492b629957393459773b0f6993c08bcc8a
Austin Kumbera <austin.kumbera@gmail.com>
2014-05-15 01:29:56 -0600

    Game now runs.

--- tictac.py
+++ tictac.py
@@ -6,13 +6,42 @@ def draw_board(state):
 	print state[6] + '|' + state[7] + '|' + state[8]
 
 def input_board(y):
-	pass
+	if game_state.xturn==True:
+		game_state.board[y]='X'
+		game_state.xturn=False
+	else:
+		game_state.board[y]='O'
+		game_state.xturn=True
 
 def check_victory():
 	pass
 
-class turn():
-	pass
-
 def new_game():
-	state=[' ',' ',' ',' ',' ',' ',' ',' ',' ']
+	game_state.board=[' ',' ',' ',' ',' ',' ',' ',' ',' ']
+	game_state.game_over=False
+
+def quit_game():
+	game_state.game_over=True
+	print 'Thank you for playing.'
+
+class game_state():
+	board = [' ',' ',' ',' ',' ',' ',' ',' ',' ']
+	xturn = True
+	game_over = False
+
+def run_game():
+	while game_state.game_over==False:
+		s = int(raw_input('Please select a location: '))
+		if s==0 or s==1 or s==2 or s==3 or s==4 or s==5 or s==6 or s==7 or s==8:
+			if game_state.board[s]==' ':
+				input_board(s)
+				draw_board(game_state.board)
+				check_victory()
+			else:
+				print 'Error: location not empty.'
+		elif s==9:
+			quit_game()
+		else:
+			print 'Please enter a valid command.'
+
+run_game()
